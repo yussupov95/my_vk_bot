bfa4e09b11a7b65f5ba5a6c368168d2cb31066ee
yussupov95 <mukhammadyussu@gmail.com>
2026-03-13 20:23:07 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -163,7 +163,7 @@ async def menu_navigation(message: Message):
         await message.answer("Оставь отзыв здесь: https://vk.com/wall-236560135_7")
     
     elif text == "💬 Наш чат":
-        await message.answer("Присоединяйся к чату: https://vk.me/join/V0Th6yX2jAgaZX1Kmcum2M9togNPA1NCqU=")
+        await message.answer("Присоединяйся к чату: https://vk.me/join/V0Th6yX2jAgaZX1KMcum2W9togWPAlNCqJU=")
 
     elif text == "💰 Благотворительность":
         await message.answer(
