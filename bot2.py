1577ad0cc8648b1e7ebb96b671cb6453da7c6180
yussupov95 <mukhammadyussu@gmail.com>
2026-03-13 15:09:42 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -199,7 +199,7 @@ Video.id
 video.id
 
 
-elif text == "💰 Благотворительность":
+    elif text == "💰 Благотворительность":
         await message.answer(
             f"💰 Номер карты Сбера:\n`2202 2081 4442 2046`\n\n"
             f"После перевода нажми кнопку ниже, чтобы сообщить мне.",
@@ -312,3 +312,4 @@ async def unknown_handler(message: Message):
 if __name__ == "__main__":
     print("✅ Бот запущен и ждёт сообщения...")
     bot.run_forever()
+
